835a97e1375ce224dcfc2c54a70d06d64479492d
narogongpride <wsarfk@gmail.com>
2021-08-31 22:13:26 +0700

    first commit feat/register-form

---
+++ src/RegisterForm.js
@@ -0,0 +1,20 @@
+import React from "react";
+// import "./RegisterForm.css";
+
+const RegisterForm = () => {
+  return (
+    <div className="register-container">
+      <h2>Register Form</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+        nostrum veritatis natus rem repudiandae, beatae provident officiis
+        repellendus nesciunt similique consequatur maxime sunt quibusdam neque
+        esse, aliquid illo sequi! Eaque autem tenetur quaerat minus libero,
+        itaque dolorem culpa, maxime officiis omnis ex ab soluta optio sint
+        alias exercitationem pariatur non.
+      </p>
+    </div>
+  );
+};
+
+export default RegisterForm;
