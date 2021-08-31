08fab031826932d1f25ed83fc45c5f4f5aa085ab
narogongpride <wsarfk@gmail.com>
2021-08-31 22:21:14 +0700

    First commit registrer form

--- src/RegisterForm.js
+++ src/RegisterForm.js
@@ -7,11 +7,7 @@ const RegisterForm = () => {
       <h2>Register Form</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
-        nostrum veritatis natus rem repudiandae, beatae provident officiis
-        repellendus nesciunt similique consequatur maxime sunt quibusdam neque
-        esse, aliquid illo sequi! Eaque autem tenetur quaerat minus libero,
-        itaque dolorem culpa, maxime officiis omnis ex ab soluta optio sint
-        alias exercitationem pariatur non.
+        nostrum veritatis natus rem repudiandae.
       </p>
     </div>
   );
