d93a531d77351d15490c1b2845ece76f3c26d219
Inestiya <inestiyakhoirunisa@gmail.com>
2021-09-01 23:29:08 +0700

    Crowd tim A

---
+++ Prod-Crowd-Academy-A/src/Sidebar.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Sidebar = ({ activeSidebar }) => {
+  return (
+    <div>
+      <div id="sidebar" className={activeSidebar ? "active" : ""}>
+        <div className="logo">
+          <a href="/">BANK JAGO</a>
+        </div>
+        <div className="nav">
+          {/* Menu sidebar bisa disesuaikan sesuai tugas individunya */}
+          <a href="/home">Beranda</a>
+          <a href="/my-profile">Profil Saya</a>
+          <a href="/addnewclass">Buat Kelas Baru</a>
+          <a href="/my-class">Kelas Ajar Saya</a>
+          <a href="/upload">Post Artikel</a>
+          <a href="/my-articles">Artikel Saya</a>
+          <a href="/login">Sign Out</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
