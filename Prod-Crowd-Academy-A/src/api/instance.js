ef35aa6a97bd9e43d3290a1dabad2a74a8224385
narogongpride <wsarfk@gmail.com>
2021-10-04 21:24:54 +0700

    restapi-register

--- Prod-Crowd-Academy-A/src/api/instance.js
+++ Prod-Crowd-Academy-A/src/api/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://b32c7b00-2af0-45fe-a4e6-7c35ac0f5ed8.mock.pstmn.io",
+  baseURL: "https://cedae9f5-4c43-4c14-b228-da5f76758943.mock.pstmn.io",
 });
 
 export { instance };
