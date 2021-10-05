1b5cbb3ee80f81f4fd5a8923825053d009f3f62e
inestiya <43212728+inestiya@users.noreply.github.com>
2021-10-05 21:45:06 +0700

    connect to REST API by Afdil rahmat

--- src/page/Kelas.js
+++ src/page/Kelas.js
@@ -1,45 +1,129 @@
-import React from 'react'
-import { Container, Segment, Table } from 'semantic-ui-react'
+import React, {useEffect, useState } from "react";
+import { Container, Grid, Message, Loader, Dimmer } from "semantic-ui-react";
+import styled from "styled-components";
+import { instance } from "../api/instance";
 
-const Kelas = () => (
-  <Container style={{ marginTop: '2em'}}>
-    <Segment>
-    <Table style={{border: 0}}>
-    <Table.Body style={{border: 0}}>
-      <Table.Row style={{border: 0}}>
-        <Table.Cell>Jenis Kelas</Table.Cell>
-        <Table.Cell style={{width: 1}}>:</Table.Cell>
-        <Table.Cell style={{textAlign: 'left'}}>Gratis</Table.Cell>
-      </Table.Row>
-      <Table.Row style={{border: 0}}>
-        <Table.Cell>Topik Pembelajaran</Table.Cell>
-        <Table.Cell>:</Table.Cell>
-        <Table.Cell style={{textAlign: 'left'}}>IT dan Data</Table.Cell>
-      </Table.Row>
-      <Table.Row style={{border: 0}}>
-        <Table.Cell>Judul Kelas</Table.Cell>
-        <Table.Cell>:</Table.Cell>
-        <Table.Cell style={{textAlign: 'left'}}>Programming with Python</Table.Cell>
-      </Table.Row>
-      <Table.Row style={{border: 0}}>
-        <Table.Cell>Jadwal Kelas</Table.Cell>
-        <Table.Cell>:</Table.Cell>
-        <Table.Cell style={{textAlign: 'left'}}>17 Juli 2021, 10.00 WIB</Table.Cell>
-      </Table.Row>
-      <Table.Row style={{border: 0}}>
-        <Table.Cell>Deskripsi Kelas</Table.Cell>
-        <Table.Cell>:</Table.Cell>
-        <Table.Cell style={{textAlign: 'left'}}>Python adalah bahasa pemogramman yang ........................</Table.Cell>
-      </Table.Row>
-    </Table.Body>
-    </Table>
-    </Segment>
-    <Segment>
-      <Table style={{border: 0,}}>
-      <p>Kelas 2 dan selanjutnya</p>
-      </Table>
-    </Segment>
-  </Container>
-)
+/* Function with hook*/
+
+function Kelas() {
+const [response, setResponse] = useState([]);
+const [isLoading, setLoading] = useState(false);
+useEffect(() => {
+  setLoading(true);
+  instance
+    .get("/list_kelas?id_tutor=1")
+    .then((response) => {
+      setLoading(false);
+      setResponse(response.data.data)
+    })
+    .catch(() => {
+      setLoading(false);
+      <Message negative>Failed to fetch from server!</Message>
+    });
+}, []);
+
+
+  const StyledGrid = styled(Grid)`
+    border: 1px solid lightgray;
+    border-radius: 0.2rem;
+    box-shadow: 0.1rem 0.1rem lightgray;
+    margin: 2rem 0 !important;
+  `;
+
+  const StyledGridRow = styled(Grid.Row)`
+    padding: 1rem !important;
+  `;
+
+  const Read = styled.a`
+    margin-left: 0.5rem;
+    font-size: 1.2rem;
+    &:hover {
+      text-decoration: underline;
+    }
+  `;
+
+  /*combine UI Framework with styled components*/
+  return (
+    <div>
+    {isLoading ?   <Dimmer active><Loader content='Loading' /></Dimmer>  : null}
+    {response && response.map((kelas) => {
+      return (
+        <div>
+   <Container>
+      <StyledGrid>
+        <StyledGridRow>
+          <Grid.Column width={6}>
+            <span>Jenis Kelas</span>
+          </Grid.Column>
+          <Grid.Column width={1}>
+            <span>:</span>
+          </Grid.Column>
+          <Grid.Column width={3}>
+            <span>{kelas.jenis_kelas}</span>
+          </Grid.Column>
+        </StyledGridRow>
+
+        <StyledGridRow>
+          <Grid.Column width={6}>
+            <span>Topik Pembelajaran</span>
+          </Grid.Column>
+          <Grid.Column width={1}>
+            <span>:</span>
+          </Grid.Column>
+          <Grid.Column width={3}>
+            <span>{kelas.topik_pembelajaran}</span>
+          </Grid.Column>
+        </StyledGridRow>
+
+        <StyledGridRow>
+          <Grid.Column width={6}>
+            <span>Judul Kelas</span>
+          </Grid.Column>
+          <Grid.Column width={1}>
+            <span>:</span>
+          </Grid.Column>
+          <Grid.Column width={3}>
+            <span>{kelas.judul_kelas}</span>
+          </Grid.Column>
+        </StyledGridRow>
+
+        <StyledGridRow>
+          <Grid.Column width={6}>
+            <span>Jadwal Kelas</span>
+          </Grid.Column>
+          <Grid.Column width={1}>
+            <span>:</span>
+          </Grid.Column>
+          <Grid.Column width={3}>
+          <span>{kelas.jadwal_kelas}</span>
+          </Grid.Column>
+        </StyledGridRow>
+
+        <StyledGridRow>
+          <Grid.Column width={6}>
+            <span>Deskripsi Kelas</span>
+          </Grid.Column>
+          <Grid.Column width={1}>
+            <span>:</span>
+          </Grid.Column>
+          <Grid.Column width={3}>
+          <span>{kelas.deskripsi_kelas}</span>
+          </Grid.Column>
+        </StyledGridRow>
+      </StyledGrid>
+
+      <StyledGrid>
+        <StyledGridRow>
+          <Grid.Column>
+            <span>Isi Kelas 2 dan Seterusnya</span>
+          </Grid.Column>
+        </StyledGridRow>
+      </StyledGrid>
+    </Container>
+    </div>)}
+    )}
+    </div>
+  );
+};
 
 export default Kelas;
