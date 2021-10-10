d6dc351b8caaae0dc5cef3e85837800edb6aab53
Inestiya <inestiyakhoirunisa@gmail.com>
2021-10-10 07:20:36 +0700

    end to end test

--- cypress/integration/2-advanced-examples/login.spec.js
+++ cypress/integration/2-advanced-examples/login.spec.js
@@ -1,3 +1,4 @@
+Cypress.on('uncaught:exception', (err, runnable) => { return false; })
 describe ('Login page auth app', ()=> {
     it ('succcesfuly loads', () => {
         cy.visit('http://localhost:3000/login');
--- cypress/integration/2-advanced-examples/register.spec.js
+++ cypress/integration/2-advanced-examples/register.spec.js
@@ -4,12 +4,12 @@ describe ('Register page auth app', ()=> {
     });
     it  ('Register correctly', () => {
     cy.visit('http://localhost:3000/register');
-    cy.get("input[id='email']").type('timA@mail.com');
-    cy.wait(1000);
     cy.get('.query-form').within(() => {
         cy.get('input:first').should('have.attr', 'placeholder', 'Isi username atau email')
         cy.get('input:last').should('have.attr', 'placeholder', 'Isi Ulang Password')
-    })
+    });
+    cy.get("input[id='email']").type('timA@mail.com');
+    cy.wait(1000);
     cy.get("input[id='password']").type('password');
     cy.wait(1000);
     cy.get("input[id='password']").type('password');
